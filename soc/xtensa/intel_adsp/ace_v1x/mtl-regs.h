f597ee9c48e10b23f3d026a81295244299c5ec75
Anas Nashif <anas.nashif@intel.com>
2022-01-28 15:19:19 -0800

    intel_adsp: add a new series to support Meteorlake
    Meteorlake support as part of the Intel ADSP family.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    Co-authored-by: Michal Wasko <michal.wasko@intel.com>
    Co-authored-by: Konrad Leszczynski <konrad.leszczynski@intel.com>
    Co-authored-by: Rafal Redzimski <rafal.f.redzimski@intel.com>
    Co-authored-by: Enjia Mai <enjia.mai@intel.com>
    Co-authored-by: Flavio Ceolin <flavio.ceolin@intel.com>
    Co-authored-by: Tomasz Leman <tomasz.m.leman@intel.com>
    Co-authored-by: Bonislawski Adrian <adrian.bonislawski@intel.com>
    Co-authored-by: Serhiy Katsyuba <serhiy.katsyuba@intel.com>
    Co-authored-by: Andrey Borisovich <andrey.borisovich@intel.com>

---
+++ soc/xtensa/intel_adsp/ace_v1x/mtl-regs.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2022 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _ZEPHYR_SOC_INTEL_ADSP_MTL_REGS_H_
+#define _ZEPHYR_SOC_INTEL_ADSP_MTL_REGS_H_
+
+/* mtl-regs.h is deprecated, use ace_v1x-regs.h instead */
+
+#include <ace_v1x-regs.h>
+
+#endif /* _ZEPHYR_SOC_INTEL_ADSP_MTL_REGS_H_ */
