04950cba4cd07c8121c992501c9ceab3e09cf048
Anas Nashif <anas.nashif@intel.com>
2022-02-14 22:15:06 -0500

    meteorlake: sim: do not flash, this is now an emulator

--- boards/xtensa/intel_adsp_ace15_mtpm/board.cmake
+++ boards/xtensa/intel_adsp_ace15_mtpm/board.cmake
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher_ifnset(misc-flasher)
-board_runner_args(misc-flasher "${BOARD_DIR}/support/zacewrap.py")
-board_finalize_runner_args(misc-flasher)
+set(SUPPORTED_EMU_PLATFORMS mtlsim)
+
 board_set_rimage_target(mtl)
