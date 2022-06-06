b7373b63777827c33f86d1aa65d40e8f62dfae2e
Andrey Borisovich <andrey.borisovich@intel.com>
2022-06-06 20:42:42 +0200

    intel_adsp_mtl_sim: support west flash
    Move scripting to board's support directly and enable misc-flasher to
    call wrapper script directly.
    
    Now you can just call `west flash` after building.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- boards/xtensa/intel_adsp_mtl/board.cmake
+++ boards/xtensa/intel_adsp_mtl/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_set_flasher_ifnset(misc-flasher)
+board_runner_args(misc-flasher "${BOARD_DIR}/support/zacewrap.py")
 board_finalize_runner_args(misc-flasher)
-
 board_set_rimage_target(mtl)
